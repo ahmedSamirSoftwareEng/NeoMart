3f59c116137aab71538599e31c3ada83b3b0ce0c
ahmedsamirIbrahim <ahmedsamirsoftwareengineer@gmail.com>
2025-06-02 01:59:18 +0300

    feat: add order management models, migrations, and styles
    - Introduced `Order`, `OrderItem`, and `OrderAddress` models with basic relationships and fillable fields
    - Created migrations for `orders`, `order_items`, and `order_addresses` tables
    - Updated `carts` table migration to add unique constraint on `cookie_id` and `product_id`
    - Added `.break-all` utility class to CSS for breaking long words

---
+++ app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'store_id',
+        'status',
+        'payment_method',
+        'payment_status',
+    ];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault(
+            [
+                'name' => 'Guest Customer',
+            ]
+        );
+    }
+}
