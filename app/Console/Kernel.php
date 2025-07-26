89cfcb9ae3cbc9ad135fdf18c67ed6de96247e3f
ahmedSamirSoftwareEng <ahmedsamirsoftwareengineer@gmail.com>
2025-07-26 13:12:27 +0300

    Add scheduled job to delete expired pending orders every two minutes
    - Created `DeleteExpiredOrders` job to delete pending orders older than 7 days.
    - Registered the job in `Kernel.php` to run every two minutes using Laravel's scheduler.
    - Minor formatting change in `commands()` method in `Kernel.php` for consistency.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\DeleteExpiredOrders;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +17,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->job(new DeleteExpiredOrders())->everyTwoMinutes();
     }
 
     /**
@@ -25,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
